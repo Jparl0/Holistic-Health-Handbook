4da7255e48666f63e652c60c791437445c2e01e5
Joseph <joeyparlamenti@yahoo.com>
2024-08-08 23:49:46 -0400

    mental typeing addition

--- src/pages/physical/types.tsx
+++ src/pages/physical/types.tsx
@@ -2,28 +2,31 @@ import React, { Component } from 'react'
 
 type physicalTypes = {
     wellnessName: string
-    age: number
     physNum: number
+    age: number
+    maxHR: number
+    cardioNum: number
     flexNum: number
     mobilityNum: number
-    cardioNum: number
     strengthNum: number
-    maxHR: number
-    immunalNum: number
-    coordinationNum: number
     nutritionalNum:number
+    coordinationNum: number
+    immunalNum?: number
 }
 
+ 
 type State = {
-    
+
 }
 
-class types extends Component<physicalTypes, State> {
+class types extends PhysicalWellness<physicalTypes, State> {
   state = {}
 
   render() {
     return (
-      <div>types</div>
+      <div>
+
+      </div>
     )
   }
 }
