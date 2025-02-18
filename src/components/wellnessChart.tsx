25bab4e5962d34a909a347941cb50f7f87857455
Joey Parlamenti <joeyparlamenti@gmail.com>
2025-02-17 21:50:01 -0500

    additional files for introduction of cumulative wellness page

---
+++ src/components/wellnessChart.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+type Props = {}
+
+export default function wellnessChart({}: Props) {
+  return (
+    <div >
+        <section>
+            <h3> Title</h3>
+            <div>outer container
+                <div> inner container
+                    <form>
+                        
+                    </form>
+                </div>
+            </div>
+        </section>
+      </div>
+    )
+  )
+}
---
+++ src/utils/quizData.tsx
@@ -0,0 +1,18 @@
+import React, { Component, SetStateAction, useState } from 'react'
+type Props = {
+    
+}
+
+type State = {
+
+}
+
+
+export default class quizData extends Component<Props, State> {
+  render() {
+    return (
+      <div>
+        
+      </div>
+  }
+}
